5fdd3bae916f9f90f30b502238c406ce12d381a3
sneufville <sneufville@academic.rrc.ca>
2024-06-22 02:30:47 -0500

    update site data for developer setup page

--- src/pages/DeveloperSetupPage.tsx
+++ src/pages/DeveloperSetupPage.tsx
@@ -4,9 +4,12 @@
  * project  coding-assignment-14-cra
  */
 import React from "react";
+import { HiWrenchScrewdriver } from "react-icons/hi2";
+import { FaCheck } from "react-icons/fa";
 import PageWrapper from "../components/PageWrapper/PageWrapper";
 import { HeroImage } from "../components/HeroImage";
 import siteData from "../siteData.json";
+import { Label } from "../components/Label";
 
 const {
   siteSections: { developerSetup },
@@ -17,12 +20,42 @@ const DeveloperSetupPage = (): React.ReactElement => {
     <PageWrapper>
       <HeroImage
         altText={"developer setup hero image"}
-        src='../code-hero-img.jpg'
+        src='../developer-setup-img.jpg'
       />
       <h1 className='text-5xl'>Developer Setup</h1>
       <p className='leading-8'>{developerSetup.description}</p>
-      <h2>Developer Environment</h2>
-      <p>Preferred IDE: {developerSetup.ideSetup.name}</p>
+
+      {developerSetup.content.map((content, contentIndex) => (
+        <div
+          key={`${contentIndex}-${content.category}`}
+          className='flex flex-col gap-y-2 py-2'
+        >
+          <h3 className='text-2xl font-medium'>{content.category}</h3>
+          <p className='mb-4'>{content.description}</p>
+          {content.tools.map((tool, toolIndex) => (
+            <div
+              key={`${toolIndex}-${tool.name}`}
+              className='flex flex-col gap-y-2 mb-2'
+            >
+              <div className='flex items-center gap-x-1'>
+                <HiWrenchScrewdriver />
+                <h4 className='font-bold text-lg'>{tool.name}</h4>
+              </div>
+              <p className='text-lg'>{tool.description}</p>
+              <h5>Features</h5>
+              <div className='flex items-center flex-wrap gap-4'>
+                {tool?.features?.map((feature, featureIndex) => (
+                  <Label
+                    iconElement={<FaCheck />}
+                    key={`${featureIndex}-${feature}`}
+                    labelText={feature}
+                  />
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+      ))}
     </PageWrapper>
   );
 };
